e1a655d52ee5a8069f17ec68fd77855331847459
Stefan Ivanov <135533109+Stef40fit@users.noreply.github.com>
2023-11-02 20:42:49 +0200

    Add project files.

---
+++ 02.Average Last Elements/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = int.Parse(Console.ReadLine());
+double currentSum = 0;
+for(int i = 0; i < n; i++)
+{
+    int currentNum = numbers[numbers.Length - 1 - i];
+     currentSum +=currentNum;
+
+}
+double average = currentSum / n;
+Console.WriteLine($"{average:f2}");
